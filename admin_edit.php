0c023e634eb3426d549190cb1775b8d56fcf8458
Andreea Simona Ciobanu <andreeasimciobanu1@gmail.com>
2023-11-10 11:50:28 +0200

    Practice App

--- admin_edit.php
+++ admin_edit.php
@@ -13,16 +13,13 @@
   session_start();
 
   $productTitle = $productDescription = $productPrice = '';
-  $productId = null;
+  $productId = isset($_GET['product_id']) ? $_GET['product_id'] : null;
 
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_GET['product_id'])) {
-
+ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $productId !== null) {
       //edit exiting product with the product_id selected from admin.php page
-      $productId = $_GET['product_id'];
-      $productTitle = isset($_POST['product_title']) ? $_POST['product_title'] : '';
-      $productDescription = isset($_POST['product_description']) ? $_POST['product_description'] : '';
-      $productPrice = isset($_POST['product_price']) ? $_POST['product_price'] : '';
+      $productTitle = $_POST['product_title'] ?? '';
+      $productDescription = $_POST['product_description'] ?? '';
+      $productPrice = $_POST['product_price'] ?? '';
 
       //update product in db
       $sql = "UPDATE products SET product_title=?, product_description=?, product_price=? WHERE product_id=?";
@@ -31,30 +28,24 @@
       header("Location: admin.php");
       // echo "Product updated successfully";
     } 
-
-  } 
-  if ($_SERVER['REQUEST_METHOD'] == "GET") {
-    if (isset($_GET['product_id'])) {
+  
+  if ($_SERVER['REQUEST_METHOD'] == "GET" && $productId !== null) 
+  {
       //fetch product data for editing
-
-      $productId = $_GET['product_id'];
       $sql = "SELECT * FROM products WHERE product_id = ?";
       $stmt = $conn->prepare($sql);
       $stmt->execute([$productId]);
       $productData = $stmt->fetch(PDO::FETCH_ASSOC);
 
-      if ($productData) {
+      if ($productData) 
+      {
         $productTitle = $productData['product_title'];
         $productDescription = $productData['product_description'];
         $productPrice = $productData['product_price'];
       }
     }
-  }
-
-
-  // Display the form with the product data for editing
-
   ?>
+  <!-- Display the form with the product data for editing -->
 
   <form action="admin_edit.php<?php if (isset($productId)) echo '?product_id=' . $productId; ?>" method="POST" enctype="multipart/form-data">
     <div class="add-products-container">
@@ -72,7 +63,6 @@
 
     <input type="submit" value="Update Product" name="submit-edit-product">
   </form>
-
 
 </body>
 
